df898f5a69d03ce99cda698308d57bd63a73bea3
Chris Fernandes <chrisfernandes@Chriss-MacBook-Air.local>
2015-08-26 15:11:55 -0400

    Working Rover Code

--- Rover.rb
+++ Rover.rb
@@ -1,34 +1,104 @@
 # Exercise 2 - Mars Rover!!
 
 
+#Gets the plateau size
+
+print "Enter plateau size: "
+
+plateau = gets.chomp.split.map {|x| x.to_i }
+max_x = plateau[0]
+max_y = plateau[1]
+puts "#{max_x}, #{max_y}"
+
+
+# Gets the initial coordinates
+
+print "Enter Initial Coordinates and Direction: "
+
+initial_coords = gets.chomp.split
+x = initial_coords[0].to_i
+y = initial_coords[1].to_i
+init_direction = initial_coords[2]
+
+
 class Rover
 
-  def initialize (x_coordinate, y_coordinate, direction)
+attr_accessor :current_direction, :d, :compass, :com_length, :rover_input, :count
 
-    @x_coord = x_coordinate
-    @y_coord = y_coordinate
-    @direc = direction
+# Takes in initial directiokn
+def initialize(x_coord, y_coord, direction)
+
+  @compass = ["N", "E", "S", "W"]
+  @count = 0
+  @x = x_coord
+  @y = y_coord
+  @d = direction
+  @current_direction = @compass.index(@d)
+  puts "\nThe initial position of the robot is  #{@x}, #{@y}. It is facing the #{@d} direction."
   end
 
-  def read_instruction(instruct)
 
-    puts "Please enter an L to turn 90 degrees left, R to move
-          90 degrees right, or M to move forward by one unit."
+def read_instruction
 
-          if "L"
-
-            #Send signal to turn left
-
-          elsif "R"
-            #Send signal to turn right
-
-          elsif "M"
-            #Send signal to move forward one unit
-              end
-
-    end
-
+puts "\nEnter a command string. L will turn the rover 90 degrees left, R will turn it 90 degrees right, and M will move the rover one unit forward."
+@rover_input = gets.chomp.split("")
+@com_length = rover_input.length
 end
 
 
-direction = [N, W, S, E]
+def turn(revolve)
+
+if revolve == "L"
+@compass.rotate!(-1)
+
+elsif revolve == "R"
+@compass.rotate!(1)
+end
+end
+
+
+
+def move
+
+if @compass[@current_direction] == "N"
+  @y += 1
+
+elsif @compass[@current_direction] == "E"
+  @x += 1
+
+elsif @compass[@current_direction] == "S"
+  @y -= 1
+
+elsif @compass[@current_direction] == "W"
+  @x -= 1
+
+  end
+end
+
+def execute_instruction
+
+  while @count < (@com_length)
+
+    if @rover_input[@count] == "L"
+      turn("L")
+
+    elsif @rover_input[@count] == "R"
+      turn("R")
+
+    elsif @rover_input[@count] == "M"
+      move
+    end
+
+    @count += 1
+  end
+
+
+  puts "Final position is #{@x}, #{@y}, and the direction is #{@compass[@current_direction]}"
+  end
+end
+
+
+rov1 = Rover.new(x, y, init_direction)
+rov1.read_instruction
+rov1.execute_instruction
+
